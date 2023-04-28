15d1ad38b487dc5876376b0620e842d88047f631
carlinhocr <carlos.rienzi@gmail.com>
2023-04-27 22:31:49 -0300

    empezando clase objetos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cstdio>
 #include "ObjetoJuego.h"
+#include <cstdio>
 
 int main(){
     ObjetoJuego juego = ObjetoJuego("espada_laser","gran objeto",true);
     std::cout << juego.mostrarNombre();
     std::cout << juego.mostrarDescripcion();
     std::cout << juego.isRemovable();
-}
+};
