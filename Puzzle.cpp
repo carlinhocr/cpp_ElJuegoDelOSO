a15f363060b4246685f9c86d3eb083d38b6d8a63
carlinhocr <carlos.rienzi@gmail.com>
2023-05-03 20:11:38 -0300

    empezando clase puzzle

---
+++ Puzzle.cpp
@@ -0,0 +1,36 @@
+/*
+
+class Puzzle(object):
+
+    def __init__(self, objeto1, objeto2, accionResuelve, descripcionNoResuelto, descripcionResuelto):
+        self.objeto1 = objeto1
+        self.objeto2 = objeto2
+        self.accionResuelve = accionResuelve
+        self.resuelto = False
+        self.descripcionNoResuelto = descripcionNoResuelto
+        self.descripcionResuelto = descripcionResuelto
+
+    def mostrarDescripcion(self):
+        if self.resuelto:
+            descripcion = self.descripcionResuelto
+        else:
+            descripcion = self.descripcionNoResuelto
+        return descripcion
+
+    def mostrarAccion(self):
+        return self.accionResuelve
+
+    def resolverPuzzle(self):
+        self.resuelto=True
+        return self.descripcionResuelto
+
+    def mostrarNombreObjeto1(self):
+        return self.objeto1.mostrarNombre()
+
+    def mostrarNombreObjeto2(self):
+        return self.objeto2.mostrarNombre()
+
+    def statusSolved(self):
+        return self.resuelto
+
+    */
---
+++ Puzzle.h
@@ -0,0 +1,58 @@
+/*
+
+class Puzzle(object):
+
+    def __init__(self, objeto1, objeto2, accionResuelve, descripcionNoResuelto, descripcionResuelto):
+        self.objeto1 = objeto1
+        self.objeto2 = objeto2
+        self.accionResuelve = accionResuelve
+        self.resuelto = False
+        self.descripcionNoResuelto = descripcionNoResuelto
+        self.descripcionResuelto = descripcionResuelto
+
+    def mostrarDescripcion(self):
+        if self.resuelto:
+            descripcion = self.descripcionResuelto
+        else:
+            descripcion = self.descripcionNoResuelto
+        return descripcion
+
+    def mostrarAccion(self):
+        return self.accionResuelve
+
+    def resolverPuzzle(self):
+        self.resuelto=True
+        return self.descripcionResuelto
+
+    def mostrarNombreObjeto1(self):
+        return self.objeto1.mostrarNombre()
+
+    def mostrarNombreObjeto2(self):
+        return self.objeto2.mostrarNombre()
+
+    def statusSolved(self):
+        return self.resuelto
+
+    */
+
+#ifndef Puzzle_h
+#define Puzzle_h
+#include <iostream>
+
+class Puzzle {
+
+    public:
+            Puzzle(std::string objeto1, std::string objeto2, 
+            std::string accionResuelve, std::string descripcionNoResuelto, 
+            std::string descripcionResuelto);
+            std::string mostrarDescripcion();
+            std::string mostrarAccion();
+            bool resolverPuzzle();
+            std::string mostrarNombreObjeto1();
+            std::string mostrarNombreObjeto2();
+            bool statusSolved();
+
+    private:
+}
+
+#endif
